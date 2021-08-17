cfb178dd6808e714d33228bcb29f7d88ee9b36d6
Tikhamporn Tepsut <tikhamporntan@gmail.com>
2021-08-17 20:51:12 +0700

    Tan First commit

--- src/first.java
+++ src/first.java
@@ -1,6 +1,8 @@
 
 public class first {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("Y.T.D");
+		System.out.println("Y.S.S");
+		System.out.println("R.Y.N");
 	}
 }
